171e89573bcd710c937c48cc1517147df6d19c9c
manaswita <manaswita.ch10@gmail.com>
2024-09-01 01:20:39 +0530

    next.config updated

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,24 @@ const nextConfig = {
   // experimental: {
   //   appDir: true,
   // },
-}
+};
 
-export default nextConfig;
+const webpackConfig = (config, { isServer }) => {
+  if (!isServer) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+  }
+  return config;
+};
+
+// Assign to a variable before exporting
+const finalConfig = {
+  ...nextConfig,
+  webpack: webpackConfig,
+};
+
+export default finalConfig;
