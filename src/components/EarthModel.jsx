b3863e75a4a9d705def1a43d527678fcedd74325
manaswita <manaswita.ch10@gmail.com>
2024-09-01 00:56:51 +0530

    updated EarthModel code

--- src/components/EarthModel.jsx
+++ src/components/EarthModel.jsx
@@ -1,106 +1,115 @@
 'use client';  // Mark this component as a Client Component
 
 import { useEffect, useRef } from "react";
-import * as echarts from 'echarts';
-import 'echarts-gl';
-import { useRouter } from 'next/router';  // Use next/navigation instead of next/router
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';  // Use next/navigation instead of next/router
 
-function EarthModel() {
+const EarthModel = () => {
   const chartRef = useRef(null);
   const router = useRouter();
 
   useEffect(() => {
-    const chartDom = chartRef.current;
-    const myChart = echarts.init(chartDom);
+    let myChart;
 
-    const option = {
-      backgroundColor: "#000",
-      globe: {
-        baseTexture: '/assets/earth.jpg',  // Use relative URL path
-        heightTexture: '/assets/earth.jpg',  // Use relative URL path
-        environment: '/assets/starfield.jpg',  // Use relative URL path
-        shading: "lambert",
-        atmosphere: {
-          show: true
-        },
-        light: {
-          ambient: {
-            intensity: 0.1,
+    // Ensure that echarts and echarts-gl are only loaded in the client
+    if (typeof window !== 'undefined') {
+      const echarts = require('echarts');
+      require('echarts-gl');
+  
+      const chartDom = chartRef.current;
+      myChart = echarts.init(chartDom);
+
+      const option = {
+        backgroundColor: "#000",
+        globe: {
+          baseTexture: '/assets/earth.jpg',  // Use relative URL path
+          heightTexture: '/assets/earth.jpg',  // Use relative URL path
+          environment: '/assets/starfield.jpg',  // Use relative URL path
+          shading: "lambert",
+          atmosphere: {
+            show: true
           },
-          main: {
-            intensity: 1
+          light: {
+            ambient: {
+              intensity: 0.1,
+            },
+            main: {
+              intensity: 1
+            }
           }
-        }
-      },
-      series: [
-        {
-          type: 'scatter3D',
-          coordinateSystem: 'globe',
-          blendMode: 'lighter',
-          symbol: 'image:///assets/pin.png',  // Use relative URL path
-          symbolSize: 64,
-          label: {
-            show: true,
-            formatter: '{b}',
-            position: 'right',
-            color: '#fff',
-            fontSize: 20,
-            distance: 15,
-            verticalAlign: 'middle',
-            offset: [10, 0]
-          },
-          itemStyle: {
-            color: '#ff3333',
-            borderColor: '#fff',
-            borderWidth: 2,
-            borderType: 'solid'
-          },
-          emphasis: {
+        },
+        series: [
+          {
+            type: 'scatter3D',
+            coordinateSystem: 'globe',
+            blendMode: 'lighter',
+            symbol: 'image:///assets/pin.png',  // Use relative URL path
+            symbolSize: 64,
             label: {
               show: true,
-              fontSize: 22,
-              color: '#ff6666'
+              formatter: '{b}',
+              position: 'right',
+              color: '#fff',
+              fontSize: 20,
+              distance: 15,
+              verticalAlign: 'middle',
+              offset: [10, 0]
             },
             itemStyle: {
-              color: '#ff6666'
-            }
-          },
-          data: [
-            { name: 'North America', value: [-100.0, 40.0, 1], route: '/north-america' },
-            { name: 'South America', value: [-60.0, -15.0, 1], route: '/south-america' },
-            { name: 'Europe', value: [10.0, 50.0, 1], route: '/europe' },
-            { name: 'Africa', value: [20.0, 0.0, 1], route: '/africa' },
-            { name: 'Asia', value: [100.0, 40.0, 1], route: '/asia' },
-            { name: 'Australia', value: [135.0, -25.0, 1], route: '/australia' }
-          ]
+              color: '#ff3333',
+              borderColor: '#fff',
+              borderWidth: 2,
+              borderType: 'solid'
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: 22,
+                color: '#ff6666'
+              },
+              itemStyle: {
+                color: '#ff6666'
+              }
+            },
+            data: [
+              { name: 'North America', value: [-100.0, 40.0, 1], route: '/north-america' },
+              { name: 'South America', value: [-60.0, -15.0, 1], route: '/south-america' },
+              { name: 'Europe', value: [10.0, 50.0, 1], route: '/europe' },
+              { name: 'Africa', value: [20.0, 0.0, 1], route: '/africa' },
+              { name: 'Asia', value: [100.0, 40.0, 1], route: '/asia' },
+              { name: 'Australia', value: [135.0, -25.0, 1], route: '/australia' }
+            ]
+          }
+        ]
+      };
+
+      option && myChart.setOption(option);
+
+      myChart.on('click', function (params) {
+        if (params.componentType === 'series') {
+          const clickedPin = option.series[0].data.find(d => d.name === params.name);
+          if (clickedPin) {
+            router.push(clickedPin.route);
+          }
         }
-      ]
-    };
+      });
 
-    option && myChart.setOption(option);
+      const resizeHandler = () => {
+        myChart.resize();
+      };
 
-    myChart.on('click', function (params) {
-      if (params.componentType === 'series') {
-        const clickedPin = option.series[0].data.find(d => d.name === params.name);
-        if (clickedPin) {
-          router.push(clickedPin.route);
-        }
-      }
-    });
-
-    const resizeHandler = () => {
-      myChart.resize();
-    };
-
-    window.addEventListener('resize', resizeHandler);
+      window.addEventListener('resize', resizeHandler);
+    }
 
     return () => {
-      myChart.dispose();
+      if (myChart) {
+        myChart.dispose();
+      }
       window.removeEventListener('resize', resizeHandler);
     };
   }, [router]);
 
   return <div ref={chartRef} style={{ width: "100vw", height: "100vh" }}></div>;
-}
+};
 
-export default EarthModel;
+export default dynamic(() => Promise.resolve(EarthModel), { ssr: false });
