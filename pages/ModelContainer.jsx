63174ec7bd215c940cede5fc9acc2f4569c7219c
manaswita <manaswita.ch10@gmail.com>
2024-09-04 00:57:44 +0530

    Aloevera

--- pages/ModelContainer.jsx
+++ pages/ModelContainer.jsx
@@ -65,7 +65,7 @@ export default function ModelContainer() {
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
       <Suspense fallback={<mesh><boxGeometry args={[1, 1, 1]} /><meshStandardMaterial color="blue" /></mesh>}>
-        <Model url="/assets/Asia_images/3d-models/AloeVera.gltf" />
+        <Model url="/assets/Asia_images/3d-models/Aloevera.gltf" />
       </Suspense>
       <OrbitControls />
       <axesHelper args={[5]} />
